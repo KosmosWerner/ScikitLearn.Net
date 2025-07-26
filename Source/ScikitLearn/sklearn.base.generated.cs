22176cd7622621ca721e1a71c4268d0ec9f49894
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-07-26 14:34:48 -0500

    ScikitLearn.csproj: global usings

--- Source/ScikitLearn/sklearn.base.generated.cs
+++ Source/ScikitLearn/sklearn.base.generated.cs
@@ -1,179 +1,173 @@
-using System;
-using System.Collections.Generic;
-using Numpy;
-using Python.Runtime;
+namespace ScikitLearn;
 
-namespace ScikitLearn
+public static partial class sklearn
 {
-    public static partial class sklearn
+    public static class @base
     {
-        public static class @base
+        public static PyObject clone(PyTuple estimator, bool safe = true)
         {
-            public static PyObject clone(PyTuple estimator, bool safe = true)
+            throw new NotImplementedException();
+        }
+
+        public static bool is_classifier(PyObject estimator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool is_regressor(PyObject estimator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class BaseEstimator
+        {
+            public BaseEstimator()
+            {
+            }
+
+            public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing()
             {
                 throw new NotImplementedException();
             }
 
-            public static bool is_classifier(PyObject estimator)
+            public PyDict get_params(bool deep = true)
             {
                 throw new NotImplementedException();
             }
 
-            public static bool is_regressor(PyObject estimator)
+            public BaseEstimator set_params(Dictionary<string, PyObject>? @params = null)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class BiclusterMixin
+        {
+            public BiclusterMixin()
+            {
+            }
+
+            public (NDarray<long>? , NDarray<long>? ) get_indices(int i)
             {
                 throw new NotImplementedException();
             }
 
-            public class BaseEstimator
+            public (int? , int? ) get_shape(int i)
             {
-                public BaseEstimator()
-                {
-                }
-
-                public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing()
-                {
-                    throw new NotImplementedException();
-                }
-
-                public PyDict get_params(bool deep = true)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public BaseEstimator set_params(Dictionary<string, PyObject>? @params = null)
-                {
-                    throw new NotImplementedException();
-                }
+                throw new NotImplementedException();
             }
 
-            public class BiclusterMixin
+            public NDarray get_submatrix(int i, NDarray data)
             {
-                public BiclusterMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public (NDarray<long>? , NDarray<long>? ) get_indices(int i)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public (int? , int? ) get_shape(int i)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public NDarray get_submatrix(int i, NDarray data)
-                {
-                    throw new NotImplementedException();
-                }
+        public class ClassNamePrefixFeaturesOutMixin
+        {
+            public ClassNamePrefixFeaturesOutMixin()
+            {
             }
 
-            public class ClassNamePrefixFeaturesOutMixin
+            public PyObject get_feature_names_out(NDarray? input_features = null)
             {
-                public ClassNamePrefixFeaturesOutMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public PyObject get_feature_names_out(NDarray? input_features = null)
-                {
-                    throw new NotImplementedException();
-                }
+        public class ClassifierMixin
+        {
+            public ClassifierMixin()
+            {
             }
 
-            public class ClassifierMixin
+            public float score(NDarray X, NDarray y, NDarray? sample_weight = null)
             {
-                public ClassifierMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public float score(NDarray X, NDarray y, NDarray? sample_weight = null)
-                {
-                    throw new NotImplementedException();
-                }
+        public class ClusterMixin
+        {
+            public ClusterMixin()
+            {
             }
 
-            public class ClusterMixin
+            public NDarray<long> fit_predict(NDarray X, Dictionary<string, PyObject>? @params = null)
             {
-                public ClusterMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public NDarray<long> fit_predict(NDarray X, Dictionary<string, PyObject>? @params = null)
-                {
-                    throw new NotImplementedException();
-                }
+        public class DensityMixin
+        {
+            public DensityMixin()
+            {
             }
 
-            public class DensityMixin
+            public float score(NDarray X)
             {
-                public DensityMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public float score(NDarray X)
-                {
-                    throw new NotImplementedException();
-                }
+        public class MetaEstimatorMixin
+        {
+            public MetaEstimatorMixin()
+            {
+            }
+        }
+
+        public class OneToOneFeatureMixin
+        {
+            public OneToOneFeatureMixin()
+            {
             }
 
-            public class MetaEstimatorMixin
+            public PyObject get_feature_names_out(NDarray? input_features = null)
             {
-                public MetaEstimatorMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
+
+        public class OutlierMixin
+        {
+            public OutlierMixin()
+            {
             }
 
-            public class OneToOneFeatureMixin
+            public NDarray fit_predict(NDarray X, Dictionary<string, PyObject>? @params = null)
             {
-                public OneToOneFeatureMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public PyObject get_feature_names_out(NDarray? input_features = null)
-                {
-                    throw new NotImplementedException();
-                }
+        public class RegressorMixin
+        {
+            public RegressorMixin()
+            {
             }
 
-            public class OutlierMixin
+            public float score(NDarray X, NDarray y, NDarray? sample_weight = null)
             {
-                public OutlierMixin()
-                {
-                }
+                throw new NotImplementedException();
+            }
+        }
 
-                public NDarray fit_predict(NDarray X, Dictionary<string, PyObject>? @params = null)
-                {
-                    throw new NotImplementedException();
-                }
+        public class TransformerMixin
+        {
+            public TransformerMixin()
+            {
             }
 
-            public class RegressorMixin
+            public NDarray fit_transform(NDarray X, NDarray? y = null, Dictionary<string, PyObject>? @params = null)
             {
-                public RegressorMixin()
-                {
-                }
-
-                public float score(NDarray X, NDarray y, NDarray? sample_weight = null)
-                {
-                    throw new NotImplementedException();
-                }
+                throw new NotImplementedException();
             }
 
-            public class TransformerMixin
+            public TransformerMixin set_output(PyObject? transform = null)
             {
-                public TransformerMixin()
-                {
-                }
-
-                public NDarray fit_transform(NDarray X, NDarray? y = null, Dictionary<string, PyObject>? @params = null)
-                {
-                    throw new NotImplementedException();
-                }
-
-                public TransformerMixin set_output(PyObject? transform = null)
-                {
-                    throw new NotImplementedException();
-                }
+                throw new NotImplementedException();
             }
         }
     }

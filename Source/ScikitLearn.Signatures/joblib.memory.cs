fb04f1eeccc3e3ddfaa4a4a7101c031bec7d0231
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-08-01 19:10:23 -0500

    joblib.memory.cs: Created for interoperability with Pipeline

---
+++ Source/ScikitLearn.Signatures/joblib.memory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScikitLearn;
+
+public static partial class joblib
+{
+    [Module]
+    public static class memory
+    {
+        public class Memory : PythonObject
+        {
+            public Memory(
+                string? location = null,
+                string backend = "local",
+                string? mmap_mode = null,
+                bool compress = false,
+                int verbose = 1,
+                int? bytes_limit = null,
+                Dictionary<string, PyObject>? backend_options = null)
+            { }
+
+            public void clear(bool warn = true) { }
+        }
+    }
+}

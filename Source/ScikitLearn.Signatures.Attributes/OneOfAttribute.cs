461f67c8f624671cb971dea68129df48b25fb1c1
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-08-03 18:41:49 -0500

    OneOfAttribute: Changed AllowTypes to OneOf

--- Source/ScikitLearn.Signatures.Attributes/OneOfAttribute.cs
+++ Source/ScikitLearn.Signatures.Attributes/OneOfAttribute.cs
@@ -5,18 +5,27 @@
 /// can accept values of the two specified types.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = true)]
-public class AllowedTypesAttribute<T1, T2> : Attribute { }
+public class OneOfAttribute<T1, T2> : Attribute
+{
+    public OneOfAttribute(object Default = null!) { }
+}
 
 /// <summary>
 /// Indicates that the parameter, which must be of type <see cref="object"/> or <see cref="object?"/>,  
 /// can accept values of the three specified types.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = true)]
-public class AllowedTypesAttribute<T1, T2, T3> : Attribute { }
+public class OneOfAttribute<T1, T2, T3> : Attribute
+{
+    public OneOfAttribute(object defaultValue = null!) { }
+}
 
 /// <summary>
 /// Indicates that the parameter, which must be of type <see cref="object"/> or <see cref="object?"/>,  
 /// can accept values of the four specified types.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = true)]
-public class AllowedTypesAttribute<T1, T2, T3, T4> : Attribute { }
+public class OneOfAttribute<T1, T2, T3, T4> : Attribute
+{
+    public OneOfAttribute(object defaultValue = null!) { }
+}

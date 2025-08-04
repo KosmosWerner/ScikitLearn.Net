a63f090212d5c10f2961af0846e627a4f2679025
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-08-04 09:47:20 -0500

    sklearn.isotonic.cs: Added [OneOf<>]

--- Source/ScikitLearn.Signatures/sklearn.isotonic.cs
+++ Source/ScikitLearn.Signatures/sklearn.isotonic.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using Numpy;
-using Python.Runtime;
-
 namespace ScikitLearn;
+
 public static partial class sklearn
 {
     // Classes
@@ -14,12 +10,18 @@ public static partial class sklearn
         public static bool check_increasing(NDarray x, NDarray y) => default!;
         public static NDarray isotonic_regression(NDarray y, NDarray? sample_weight = null, float? y_min = null, float? y_max = null, bool increasing = true) => default!;
         // Classes
-        public class IsotonicRegression : PythonObject
+        public class IsotonicRegression :
+            @base.RegressorMixin,
+            @base.TransformerMixin<IsotonicRegression>,
+            @base.BaseEstimator<IsotonicRegression>
         {
             // Constructor
-            public IsotonicRegression(float? y_min = null, float? y_max = null, bool increasing = true, string out_of_bounds = "nan")
-            {
-            }
+            public IsotonicRegression(
+                float? y_min = null,
+                float? y_max = null,
+                [OneOf<bool, string>(Default: true)] object increasing = default!,
+                string out_of_bounds = "nan")
+            { }
 
             // Properties
             public float X_min_ => default!;
@@ -30,26 +32,19 @@ public static partial class sklearn
             public bool increasing_ => default!;
 
             // Methods
-            [Self]
-            public IsotonicRegression fit(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
+            [Self] public IsotonicRegression fit(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
             public NDarray predict(NDarray T) => default!;
             public float score(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
-            [Self]
-            public IsotonicRegression set_fit_request(string? sample_weight = "$UNCHANGED$") => default!;
-            [Self]
-            public IsotonicRegression set_output(string? transform = null) => default!;
-            [Self]
-            public IsotonicRegression set_params(params (string key, object value)[] @params) => default!;
-            [Self]
-            public IsotonicRegression set_predict_request(string? T = "$UNCHANGED$") => default!;
-            [Self]
-            public IsotonicRegression set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
-            [Self]
-            public IsotonicRegression set_transform_request(string? T = "$UNCHANGED$") => default!;
+            [Self] public IsotonicRegression set_fit_request(string? sample_weight = "$UNCHANGED$") => default!;
+            [Self] public IsotonicRegression set_output(string? transform = null) => default!;
+            [Self] public IsotonicRegression set_params(params (string key, object value)[] @params) => default!;
+            [Self] public IsotonicRegression set_predict_request(string? T = "$UNCHANGED$") => default!;
+            [Self] public IsotonicRegression set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
+            [Self] public IsotonicRegression set_transform_request(string? T = "$UNCHANGED$") => default!;
             public NDarray transform(NDarray T) => default!;
         }
     }

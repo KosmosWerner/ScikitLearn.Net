4965b648adc7de4588f224b6c5b4a9cd07e77f35
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-08-04 09:06:50 -0500

    sklearn.multiclass.cs: added interfaces

--- Source/ScikitLearn.Signatures/sklearn.multiclass.cs
+++ Source/ScikitLearn.Signatures/sklearn.multiclass.cs
@@ -11,82 +11,92 @@ public static partial class sklearn
     public static class multiclass
     {
         // Classes
-        public class OneVsOneClassifier : PythonObject
+        public class OneVsOneClassifier :
+            @base.MetaEstimatorMixin,
+            @base.ClassifierMixin,
+            @base.BaseEstimator<OneVsOneClassifier>
         {
             // Constructor
-            public OneVsOneClassifier(PyObject estimator, int? n_jobs = null)
-            {
-            }
+            public OneVsOneClassifier(
+                @base.BaseEstimator estimator,
+                int? n_jobs = null)
+            { }
 
             // Properties
+            [NeedsRevision]
             public PyTuple estimators_ => default!;
             public NDarray classes_ => default!;
             public int n_classes_ => default!;
-            public PyObject pairwise_indices_ => default!;
+            [NeedsRevision]
+            public NDarray pairwise_indices_ => default!;
             public int n_features_in_ => default!;
             public NDarray feature_names_in_ => default!;
 
             // Methods
             public NDarray decision_function(NDarray X) => default!;
-            [Self]
-            public OneVsOneClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
+            [Self] public OneVsOneClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public OneVsOneClassifier partial_fit(NDarray X, NDarray y, NDarray? classes = null, params (string key, object value)[] @params) => default!;
+            [Self] public OneVsOneClassifier partial_fit(NDarray X, NDarray y, NDarray? classes = null, params (string key, object value)[] @params) => default!;
             public NDarray predict(NDarray X) => default!;
             public float score(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
-            [Self]
-            public OneVsOneClassifier set_params(params (string key, object value)[] @params) => default!;
-            [Self]
-            public OneVsOneClassifier set_partial_fit_request(string? classes = "$UNCHANGED$") => default!;
-            [Self]
-            public OneVsOneClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
+            [Self] public OneVsOneClassifier set_params(params (string key, object value)[] @params) => default!;
+            [Self] public OneVsOneClassifier set_partial_fit_request(string? classes = "$UNCHANGED$") => default!;
+            [Self] public OneVsOneClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
         }
 
-        public class OneVsRestClassifier : PythonObject
+        public class OneVsRestClassifier :
+            MultiOutputMixin,
+            @base.ClassifierMixin,
+            @base.MetaEstimatorMixin,
+            @base.BaseEstimator<OneVsRestClassifier>
         {
             // Constructor
-            public OneVsRestClassifier(PyObject estimator, int? n_jobs = null, int verbose = 0)
-            {
-            }
+            public OneVsRestClassifier(
+                @base.BaseEstimator estimator,
+                int? n_jobs = null,
+                int verbose = 0)
+            { }
 
             // Properties
+            [NeedsRevision]
             public PyTuple estimators_ => default!;
             public NDarray classes_ => default!;
             public int n_classes_ => default!;
-            public PyObject label_binarizer_ => default!;
+            public preprocessing.LabelBinarizer label_binarizer_ => default!;
             public bool multilabel_ => default!;
             public int n_features_in_ => default!;
             public NDarray feature_names_in_ => default!;
 
             // Methods
             public NDarray decision_function(NDarray X) => default!;
-            [Self]
-            public OneVsRestClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
+            [Self] public OneVsRestClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public OneVsRestClassifier partial_fit(NDarray X, NDarray y, NDarray? classes = null, params (string key, object value)[] @params) => default!;
+            [Self] public OneVsRestClassifier partial_fit(NDarray X, NDarray y, NDarray? classes = null, params (string key, object value)[] @params) => default!;
             public NDarray predict(NDarray X) => default!;
             public NDarray predict_proba(NDarray X) => default!;
             public float score(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
-            [Self]
-            public OneVsRestClassifier set_params(params (string key, object value)[] @params) => default!;
-            [Self]
-            public OneVsRestClassifier set_partial_fit_request(string? classes = "$UNCHANGED$") => default!;
-            [Self]
-            public OneVsRestClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
+            [Self] public OneVsRestClassifier set_params(params (string key, object value)[] @params) => default!;
+            [Self] public OneVsRestClassifier set_partial_fit_request(string? classes = "$UNCHANGED$") => default!;
+            [Self] public OneVsRestClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
         }
 
-        public class OutputCodeClassifier : PythonObject
+        public class OutputCodeClassifier :
+            @base.MetaEstimatorMixin,
+            @base.ClassifierMixin,
+            @base.BaseEstimator<OutputCodeClassifier>
         {
             // Constructor
-            public OutputCodeClassifier(PyObject estimator, float code_size = 1.5f, int? random_state = null, int? n_jobs = null)
-            {
-            }
+            public OutputCodeClassifier(
+                @base.BaseEstimator estimator,
+                float code_size = 1.5f,
+                int? random_state = null,
+                int? n_jobs = null)
+            { }
 
             // Properties
+            [NeedsRevision]
             public PyTuple estimators_ => default!;
             public NDarray classes_ => default!;
             public NDarray code_book_ => default!;
@@ -94,16 +104,13 @@ public static partial class sklearn
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public OutputCodeClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
+            [Self] public OutputCodeClassifier fit(NDarray X, NDarray y, params (string key, object value)[] @params) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
             public NDarray predict(NDarray X) => default!;
             public float score(NDarray X, NDarray y, NDarray? sample_weight = null) => default!;
-            [Self]
-            public OutputCodeClassifier set_params(params (string key, object value)[] @params) => default!;
-            [Self]
-            public OutputCodeClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
+            [Self] public OutputCodeClassifier set_params(params (string key, object value)[] @params) => default!;
+            [Self] public OutputCodeClassifier set_score_request(string? sample_weight = "$UNCHANGED$") => default!;
         }
     }
 }

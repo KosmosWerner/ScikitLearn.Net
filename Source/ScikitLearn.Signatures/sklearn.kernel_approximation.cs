34fa603d3e22fcb9101dd5ad9a08e9ffff192743
Kosmos Werner Heisenberg <kosmoswerner@gmail.com>
2025-08-04 09:22:20 -0500

    sklearn.kernel_aproximation.cs: Added interfaces and [OneOf<>]

--- Source/ScikitLearn.Signatures/sklearn.kernel_approximation.cs
+++ Source/ScikitLearn.Signatures/sklearn.kernel_approximation.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using Numpy;
-using Python.Runtime;
-
 namespace ScikitLearn;
+
 public static partial class sklearn
 {
     // Classes
@@ -11,37 +7,47 @@ public static partial class sklearn
     public static class kernel_approximation
     {
         // Classes
-        public class AdditiveChi2Sampler : PythonObject
+        public class AdditiveChi2Sampler :
+            @base.TransformerMixin<AdditiveChi2Sampler>,
+            @base.BaseEstimator<AdditiveChi2Sampler>
         {
             // Constructor
-            public AdditiveChi2Sampler(int sample_steps = 2, float? sample_interval = null)
-            {
-            }
+            public AdditiveChi2Sampler(
+                int sample_steps = 2,
+                float? sample_interval = null)
+            { }
 
             // Properties
             public int n_features_in_ => default!;
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public AdditiveChi2Sampler fit(NDarray X, NDarray? y = null) => default!;
+            [Self] public AdditiveChi2Sampler fit(NDarray X, NDarray? y = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public AdditiveChi2Sampler set_output(string? transform = null) => default!;
-            [Self]
-            public AdditiveChi2Sampler set_params(params (string key, object value)[] @params) => default!;
+            [Self] public AdditiveChi2Sampler set_output(string? transform = null) => default!;
+            [Self] public AdditiveChi2Sampler set_params(params (string key, object value)[] @params) => default!;
             public NDarray transform(NDarray X) => default!;
         }
 
-        public class Nystroem : PythonObject
+        public class Nystroem :
+            @base.ClassNamePrefixFeaturesOutMixin,
+            @base.TransformerMixin<Nystroem>,
+            @base.BaseEstimator<Nystroem>
         {
             // Constructor
-            public Nystroem(string kernel = "rbf", float? gamma = null, float? coef0 = null, float? degree = null, PyDict? kernel_params = null, int n_components = 100, int? random_state = null, int? n_jobs = null)
-            {
-            }
+            public Nystroem(
+                string kernel = "rbf",
+                float? gamma = null,
+                float? coef0 = null,
+                float? degree = null,
+                Dictionary<string, object>? kernel_params = null,
+                int n_components = 100,
+                int? random_state = null,
+                int? n_jobs = null)
+            { }
 
             // Properties
             public NDarray components_ => default!;
@@ -51,25 +57,29 @@ public static partial class sklearn
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public Nystroem fit(NDarray X, NDarray? y = null) => default!;
+            [Self] public Nystroem fit(NDarray X, NDarray? y = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public Nystroem set_output(string? transform = null) => default!;
-            [Self]
-            public Nystroem set_params(params (string key, object value)[] @params) => default!;
+            [Self] public Nystroem set_output(string? transform = null) => default!;
+            [Self] public Nystroem set_params(params (string key, object value)[] @params) => default!;
             public NDarray transform(NDarray X) => default!;
         }
 
-        public class PolynomialCountSketch : PythonObject
+        public class PolynomialCountSketch :
+            @base.ClassNamePrefixFeaturesOutMixin,
+            @base.TransformerMixin<PolynomialCountSketch>,
+            @base.BaseEstimator<PolynomialCountSketch>
         {
             // Constructor
-            public PolynomialCountSketch(float gamma = 1.0f, int degree = 2, int coef0 = 0, int n_components = 100, int? random_state = null)
-            {
-            }
+            public PolynomialCountSketch(
+                float gamma = 1.0f,
+                int degree = 2,
+                int coef0 = 0,
+                int n_components = 100,
+                int? random_state = null)
+            { }
 
             // Properties
             public NDarray indexHash_ => default!;
@@ -78,25 +88,27 @@ public static partial class sklearn
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public PolynomialCountSketch fit(NDarray X, NDarray? y = null) => default!;
+            [Self] public PolynomialCountSketch fit(NDarray X, NDarray? y = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public PolynomialCountSketch set_output(string? transform = null) => default!;
-            [Self]
-            public PolynomialCountSketch set_params(params (string key, object value)[] @params) => default!;
+            [Self] public PolynomialCountSketch set_output(string? transform = null) => default!;
+            [Self] public PolynomialCountSketch set_params(params (string key, object value)[] @params) => default!;
             public NDarray transform(NDarray X) => default!;
         }
 
-        public class RBFSampler : PythonObject
+        public class RBFSampler :
+            @base.ClassNamePrefixFeaturesOutMixin,
+            @base.TransformerMixin<RBFSampler>,
+            @base.BaseEstimator<RBFSampler>
         {
             // Constructor
-            public RBFSampler(float gamma = 1.0f, int n_components = 100, int? random_state = null)
-            {
-            }
+            public RBFSampler(
+                [OneOf<float, string>(Default: 1.0f)] object gamma = default!,
+                int n_components = 100,
+                int? random_state = null)
+            { }
 
             // Properties
             public NDarray random_offset_ => default!;
@@ -105,25 +117,27 @@ public static partial class sklearn
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public RBFSampler fit(NDarray X, NDarray? y = null) => default!;
+            [Self] public RBFSampler fit(NDarray X, NDarray? y = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public RBFSampler set_output(string? transform = null) => default!;
-            [Self]
-            public RBFSampler set_params(params (string key, object value)[] @params) => default!;
+            [Self] public RBFSampler set_output(string? transform = null) => default!;
+            [Self] public RBFSampler set_params(params (string key, object value)[] @params) => default!;
             public NDarray transform(NDarray X) => default!;
         }
 
-        public class SkewedChi2Sampler : PythonObject
+        public class SkewedChi2Sampler :
+            @base.ClassNamePrefixFeaturesOutMixin,
+            @base.TransformerMixin<SkewedChi2Sampler>,
+            @base.BaseEstimator<SkewedChi2Sampler>
         {
             // Constructor
-            public SkewedChi2Sampler(float skewedness = 1.0f, int n_components = 100, int? random_state = null)
-            {
-            }
+            public SkewedChi2Sampler(
+                float skewedness = 1.0f,
+                int n_components = 100,
+                int? random_state = null)
+            { }
 
             // Properties
             public NDarray random_weights_ => default!;
@@ -132,16 +146,13 @@ public static partial class sklearn
             public NDarray feature_names_in_ => default!;
 
             // Methods
-            [Self]
-            public SkewedChi2Sampler fit(NDarray X, NDarray? y = null) => default!;
+            [Self] public SkewedChi2Sampler fit(NDarray X, NDarray? y = null) => default!;
             public NDarray fit_transform(NDarray X, NDarray? y = null, params (string key, object value)[] @params) => default!;
             public NDarray<string> get_feature_names_out(NDarray<string>? input_features = null) => default!;
             public sklearn.utils.metadata_routing.MetadataRequest get_metadata_routing() => default!;
             public Dictionary<string, PyObject> get_params(bool deep = true) => default!;
-            [Self]
-            public SkewedChi2Sampler set_output(string? transform = null) => default!;
-            [Self]
-            public SkewedChi2Sampler set_params(params (string key, object value)[] @params) => default!;
+            [Self] public SkewedChi2Sampler set_output(string? transform = null) => default!;
+            [Self] public SkewedChi2Sampler set_params(params (string key, object value)[] @params) => default!;
             public NDarray transform(NDarray X) => default!;
         }
     }
